74e6ec4c17541574d60e7061a655fb7f9b92763e
suprajajagan17 <jsupraja177@gmail.com>
2025-03-06 15:33:54 +0530

    Update basicsjava.java

--- basicsjava.java
+++ basicsjava.java
@@ -1,1 +1,14 @@
-
+//Prob 1
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("enter the values ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String b = scan.next();
+        String c = scan.next();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
